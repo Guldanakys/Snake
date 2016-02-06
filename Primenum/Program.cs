9e80bd1665eb0f7ceb9c15c0a7981a984d1db7f9
guldanakys <guldanakys@gmail.com>
2016-02-06 06:23:51 +0600

    new

--- Primenum/Program.cs
+++ Primenum/Program.cs
@@ -10,10 +10,11 @@ namespace Primenum
     {
         static bool Prime(string s)
         {
-
+            // меняем строку в число
             int x = int.Parse(s);
             int cnt = 0;
 
+            // запускаем цикл для нахождения делителей
             for (int j = 2; j <= Math.Sqrt(x); ++j)
             {
                 if (x % j == 0)
@@ -21,22 +22,25 @@ namespace Primenum
                     cnt++;
                 }
             }
-
+            // возвращаем значения
             return cnt == 0 && x != 1;
         }
         static void Main(string[] args)
         {
+            // вытаскиваем числа 
             for( int i = 0; i < args.Length; ++i )
             {
                 if (Prime(args[i]))
                 {
-                    Console.WriteLine(args[i] + "is prime");
+                    Console.WriteLine(args[i] + " is prime");
                 }
                 else
                 {
-                    Console.WriteLine(args[i] + "is not prime");
+                    Console.WriteLine(args[i] + " is not prime");
                 }
             }
+
+            Console.ReadKey();
         }
     }
 }
